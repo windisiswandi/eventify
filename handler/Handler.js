3f69b0ddc7cde4e9832395ac72002763d45eecbd
windisiswandi <windisiswandi89@gmail.com>
2023-12-11 15:28:52 +0800

    update handle and routes

--- handler/Handler.js
+++ handler/Handler.js
@@ -1,5 +1,6 @@
 const conn = require('../db_config');
 
+/*========== Handler User ==========*/
 const getUsers = (req, res) => {
   conn.query('select * from users', (err, result) => {
     res.statusCode = 200;
@@ -13,6 +14,14 @@ const getUserById = (req, res) => {
     // if (result.length == 0) return res.status(404).json({ msg;  'data '})
     res.statusCode = 200;
     res.json(result);
+  });
+};
+
+const updateUser = (req, res) => {
+  const id = req.params.id;
+  conn.query('update users set ? where ?', [req.body, { id }], (err) => {
+    if (err) return res.status(500).send(err);
+    res.status(200).json({msg: "user berhasil diupdate"})
   });
 };
 
@@ -32,7 +41,7 @@ const getEventById = (req, res) => {
 
 const addEvent = (req, res) => {
   conn.query('insert into events set ?', req.body, (err) => {
-    if (err) throw err;
+    if (err) return res.status(500).send(err);
     res.status(201).json({ msg: 'Event berhasil dibuat' });
   });
 };
@@ -40,19 +49,76 @@ const addEvent = (req, res) => {
 const updateEvent = (req, res) => {
   const id = req.params.id
   conn.query('update events set ? where ?', [req.body, { id }], (err) => {
-    if (err) throw err;
+    if (err) return res.status(500).send(err);
     res.status(200).json({msg: "event berhasil diupdate"})
   });
 }
 
 const delEvents = (req, res) => {
   const id = req.params.id
-  conn.query(`delete from events where id=${id}`, (err) => {
-    if (err) throw err;
+  conn.query(`
+  DELETE events, panitia, anggota_devisi, join_event 
+  FROM events 
+  LEFT JOIN panitia ON events.kode=panitia.kode_event 
+  LEFT JOIN anggota_devisi ON panitia.id_devisi=anggota_devisi.id_devisi 
+  LEFT JOIN join_event ON events.kode=join_event.kode_event 
+  where events.id=${id}`, (err) => {
+    if (err) return res.status(500).send(err);
     res.status(200).json({ msg: 'event berhasil dihapus' });
   });
 }
-/*========== END ==========*/
+
+/*========== Handler Panitia ==========*/
+const addPanitia = (req, res) => {
+  conn.query('insert into panitia set ?', req.body, (err) => {
+    if (err) return res.status(500).send(err);
+    res.status(201).json({ msg: 'Panitia berhasil ditambahkan' });
+  })
+}
+
+const addAnggotaDevisi = (req, res) => {
+  conn.query('insert into anggota_devisi set ?', req.body, (err) => {
+    if (err) return res.status(500).send(err);
+    res.status(201).json({ msg: 'anggota berhasil ditambahkan' });
+  })
+}
+
+const updateDevisi = (req, res) => {
+  const id_devisi = req.params.id_devisi
+  conn.query('update events set ? where ?', [req.body, { id_devisi }], (err) => {
+    if (err) return res.status(500).send(err);
+    res.status(200).json({msg: "Devisi berhasil diupdate"})
+  });
+}
+
+const deleteDevisi = (req, res) => {
+  const id_devisi = req.params.id_devisi
+  conn.query(`
+  DELETE panitia, anggota_devisi 
+  FROM panitia
+  LEFT JOIN anggota_devisi ON panitia.id_devisi=anggota_devisi.id_devisi 
+  where panitia.id_devisi=${id_devisi}`, (err) => {
+    if (err) return res.status(500).send(err);
+    res.status(200).json({ msg: 'event berhasil dihapus' });
+  });
+}
+
+const delAnggotaDevisi = (req, res) => {
+  const id = req.params.id
+  conn.query(`DELETE FROM anggota_devisi where id=${id}`, (err) => {
+    if (err) return res.status(500).send(err);
+    res.status(200).json({ msg: 'anggota berhasil dihapus' });
+  });
+}
+
+/*========== Handler join event ==========*/
+const joinEvent = (req, res) => {
+  conn.query('insert into join_event set ?', req.body, (err) => {
+    if (err) return res.status(500).send(err);
+    res.status(200).json({ msg: 'Berhasil Gabung Event' });
+  })
+}
+
 
 module.exports = { handler: {
   getUsers, 
@@ -60,6 +126,13 @@ module.exports = { handler: {
   getEvents, 
   getEventById, 
   addEvent,
+  addPanitia,
+  addAnggotaDevisi,
+  updateUser,
   updateEvent,
-  delEvents
+  updateDevisi,
+  delEvents,
+  deleteDevisi,
+  delAnggotaDevisi,
+  joinEvent
 }};
--- routes/router.js
+++ routes/router.js
@@ -9,12 +9,30 @@ router.get(`${BASE_URL}/users/:id`, handler.getUserById);
 router.post(`${BASE_URL}/login`, login);
 router.post(`${BASE_URL}/register`, register);
 
+// mengambil semua event
 router.get(`${BASE_URL}/events`, handler.getEvents);
+// get event by id
 router.get(`${BASE_URL}/events/:id`, handler.getEventById);
+// tambah event
 router.post(`${BASE_URL}/events`, handler.addEvent);
+// update event
 router.put(`${BASE_URL}/events/:id`, handler.updateEvent);
+// hapus event
 router.delete(`${BASE_URL}/events/:id`, handler.delEvents);
 
-// router.post()
+// mengambil data panitia berdasarkan kode event
+router.get(`${BASE_URL}/panitia/:kode`, handler.addPanitia);
+// menambah devisi
+router.post(`${BASE_URL}/panitia`, handler.addPanitia);
+// update nama devisi
+router.put(`${BASE_URL}/panitia/:id_devisi`, handler.addPanitia);
+// menghapus devisi
+router.delete(`${BASE_URL}/panitia/:id`, handler.deleteDevisi);
+// menambah anggota devisi
+router.post(`${BASE_URL}/devisi/anggota`, handler.addAnggotaDevisi);
+// menghapus anggota devisi
+router.delete(`${BASE_URL}/devisi/anggota/:id`, handler.delAnggotaDevisi);
+// join event
+router.post(`${BASE_URL}/join/event`, handler.joinEvent)
 
 module.exports = router;
